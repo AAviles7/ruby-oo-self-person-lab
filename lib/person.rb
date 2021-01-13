581c321e00a7d170c99d8db8f8c2763ea3ff0024
John Fuquene <jfuquene@email.wm.edu>
2021-01-13 12:37:50 -0500

    John edits

--- lib/person.rb
+++ lib/person.rb
@@ -60,11 +60,30 @@ class Person
     end
 
     def work_out
-        @hygiene -= 3
-        self.hygiene=(@hygiene)
-        @happiness += 2
-        self.happiness=(@happiness)
+        self.hygiene -= 3
+        self.happiness += 2
         return '♪ another one bites the dust ♫'
     end
 
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics" 
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist" 
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return 'blah blah sun blah rain'
+        else 
+            return 'blah blah blah blah blah'
+        end
+
+
+    end 
 end
